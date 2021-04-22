e7c7dfa32e35275889df2cbe83b16636a0bb6c27
Esme Hotston Moore <ehotstonmoore@hotmail.co.uk>
2021-04-22 14:36:55 +0100

    added cat intro

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          I love cats. My favourite cat is Hello Kitty!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Miaow!</p>
       </header>
     </div>
   );
